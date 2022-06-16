cdb2eb554abcb3854352fd0d88fcee4b7d054c6d
RioRichard <thanh.huuphungak47@gmail.com>
2022-06-16 21:49:14 +0700

    Add change Role

---
+++ ScentApi2/Model/SideModel/ChangeRoleModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ScentApi2.Model.SideModel
+{
+    public class ChangeRoleModel
+    {
+        public Guid RoleId { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
